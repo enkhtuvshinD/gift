58a0e97cacad18f37407d6509b1c92c9b5162e66
enkhtuvshinD <da.enkhtuvshin@gmail.com>
2025-02-13 10:08:33 +0800

    add

--- src/GiftPage.tsx
+++ src/GiftPage.tsx
@@ -2,11 +2,9 @@ import './flower.css'; // Ensure this file is correctly placed in your styles di
 
 export default function GiftPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-pink-100">
-      <img
-        src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaWxuYTduZWluNzF6OW80b3c5ZGhieXoxN2pwenRlYWdwZjV2N296ZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vr9v03pX4cJmMx2yD8/giphy.gif"
-        alt="Bear Kiss"
-      />
-    </div>
+    <img
+      src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaWxuYTduZWluNzF6OW80b3c5ZGhieXoxN2pwenRlYWdwZjV2N296ZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vr9v03pX4cJmMx2yD8/giphy.gif"
+      alt="Bear Kiss"
+    />
   );
 }
