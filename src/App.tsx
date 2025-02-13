bdc401ff445683ad3548dc37016eb5fd3bfd23f8
enkhtuvshinD <da.enkhtuvshin@gmail.com>
2025-02-13 10:51:19 +0800

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 export default function Page() {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
+  const [pressedGift, setPressedGift] = useState(false);
+
   const yesButtonSize = noCount * 20 + 16;
 
   const handleNoClick = () => {
@@ -17,7 +19,12 @@ export default function Page() {
   console.log('1');
   return (
     <div className="-mt-16 flex h-screen flex-col items-center justify-center">
-      {yesPressed ? (
+      {pressedGift ? (
+        <img
+          src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaWxuYTduZWluNzF6OW80b3c5ZGhieXoxN2pwenRlYWdwZjV2N296ZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vr9v03pX4cJmMx2yD8/giphy.gif"
+          alt="Bear Kiss"
+        />
+      ) : yesPressed ? (
         <>
           <img
             src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"
@@ -27,7 +34,7 @@ export default function Page() {
             ❤️❤️❤️ I love you ❤️❤️❤️
           </div>
           <button
-            onClick={() => (location.href = '/gift')}
+            onClick={() => setPressedGift(true)}
             className="ml-4 rounded bg-purple-500 px-4 py-2 font-bold text-white hover:bg-purple-700"
           >
             GIFT
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import GiftPage from './GiftPage.tsx';
-
-const path = window.location.pathname;
+import './flower.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {path.includes('gift') ? <GiftPage /> : <App />}
+    <App />
   </React.StrictMode>,
 );
